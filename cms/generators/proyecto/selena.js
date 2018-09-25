c106cb9e53b5f0ecfd7a843036e20884e959bcc7
Will Alvarez <will.alvarez@gmail.com>
2018-09-25 09:28:13 -0500

    selena

---
+++ cms/generators/proyecto/selena.js
@@ -0,0 +1,36 @@
+({
+	titulo_proyecto: 'selena',
+	tipo_de_proyecto: 'tv',
+	$logo_proyecto_type: 'image',
+	logo_proyecto: '/remote/direct_uploads/1537288322_selena_logo.svg',
+	$imagen_homepage_type: 'image',
+	imagen_homepage: '/remote/direct_uploads/1537288311_selena-80.jpg',
+	$descripcion_corta_proyecto_type: 'textarea',
+	descripcion_corta_proyecto: 'EL CAOS GENERADO POR EL ASESINATO DE SELENA A MANOS DE YOLANDA, IRRUMPE EN LAS VIDAS DE MARÍA CELESTE ARRARÁS Y LA FAMILIA QUINTANILLA UNIÉNDOLAS PARA SIEMPRE CON LA DE YOLANDA.',
+	$video_fullscreen_type: 'file',
+	video_fullscreen: '',
+	genero_del_proyecto: '',
+	'año': '',
+	temporadas: '',
+	episodios: '',
+	nombre_del_cliente: '',
+	$logotipo_del_cliente_type: 'image',
+	logotipo_del_cliente: '',
+	$imagen_sinopsis_type: 'image',
+	imagen_sinopsis: '',
+	$texto_sinopsis_type: 'textarea',
+	texto_sinopsis: '',
+	trailer_url: '',
+	ver_proyecto_texto: 'ver proyecto',
+	imagenes_backstage: [
+		{
+			$imagen_type: 'image',
+			imagen: ''
+		}
+	],
+	tags: [
+		{
+			nombre_del_tag: ''
+		}
+	]
+})
