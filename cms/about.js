1928ffaa19da6deaf9ffc285dd05441038f9c005
Will Alvarez <will.alvarez@gmail.com>
2018-09-25 16:40:15 -0500

    about section integrating crew, us and clients

---
+++ cms/about.js
@@ -0,0 +1,152 @@
+({
+	$crew_min: 1,
+	crew: [
+		{
+			nombre: 'Francisco Cordero',
+			titulo: 'founder & ceo',
+			correo: '',
+			$foto_type: 'image',
+			foto: '/remote/direct_uploads/1537909583_cordero-crew.jpg',
+			$resumen_type: 'textarea',
+			resumen: ''
+		},
+		{
+			nombre: 'Oriol Uría',
+			titulo: 'productor ejecutivo',
+			correo: '',
+			$foto_type: 'image',
+			foto: '/remote/direct_uploads/1537909589_uria-crew.jpg',
+			$resumen_type: 'textarea',
+			resumen: ''
+		},
+		{
+			nombre: 'Mariano Carranco',
+			titulo: 'productor',
+			correo: 'mariano@btfmedia.com',
+			$foto_type: 'image',
+			foto: '/remote/direct_uploads/1537909572_carranco-crew.jpg',
+			$resumen_type: 'textarea',
+			resumen: 'Do dolore adipisicing est proident dolore quis labore commodo aliqua sunt nisi.'
+		},
+		{
+			nombre: 'Alejandro Aimetta',
+			titulo: 'showrunner',
+			correo: 'alejandro@btfmedia.com',
+			$foto_type: 'image',
+			foto: '/remote/direct_uploads/1537909566_aimetta-crew.jpg',
+			$resumen_type: 'textarea',
+			resumen: 'Sunt commodo culpa qui proident esse pariatur deserunt aliqua anim nulla magna pariatur consectetur do.'
+		}
+	],
+	$clients_min: 1,
+	clients: [
+		{
+			nombre: 'Amazon',
+			link: 'http://amazon.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537907916_amazon-client.svg'
+		},
+		{
+			nombre: 'Disney',
+			link: 'http://www.disney.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537907967_disney-client.svg'
+		},
+		{
+			nombre: 'Netflix',
+			link: 'http://www.netflix.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537907999_netflix-client.svg'
+		},
+		{
+			nombre: 'Youtube',
+			link: 'www.youtube.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908021_youtube-client.svg'
+		},
+		{
+			nombre: 'TNT',
+			link: 'www.tnt.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908046_tnt-client.svg'
+		},
+		{
+			nombre: 'Telemundo',
+			link: 'www.telemundo.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908083_telemundo-client.svg'
+		},
+		{
+			nombre: 'Claro',
+			link: 'www.clarovideo.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908135_claro-client.svg'
+		},
+		{
+			nombre: 'Space',
+			link: 'www.space.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908153_space-client.svg'
+		},
+		{
+			nombre: 'Univision',
+			link: 'www.univision.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908172_univision-client.svg'
+		},
+		{
+			nombre: 'El Trece',
+			link: 'www.eltrece.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908199_eltrece-client.svg'
+		},
+		{
+			nombre: 'Telefe',
+			link: 'www.telefe.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908221_telefe-client.svg'
+		},
+		{
+			nombre: 'RCN',
+			link: 'www.rcn.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908237_rcn-client.svg'
+		},
+		{
+			nombre: 'Azteca',
+			link: 'www.tvazteca.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908263_azteca-client.svg'
+		},
+		{
+			nombre: 'L',
+			link: 'www.l.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908280_ele-client.svg'
+		},
+		{
+			nombre: 'Canal Once',
+			link: 'www.oncetv.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908303_once-client.svg'
+		},
+		{
+			nombre: 'Imagen',
+			link: 'www.grupoimagen.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908326_imagen-client.svg'
+		},
+		{
+			nombre: 'Conaculta',
+			link: 'www.conaculta.gob',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908346_conaculta-client.svg'
+		},
+		{
+			nombre: 'Chile Vision',
+			link: 'www.chilevision.com',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1537908368_chile-client.svg'
+		}
+	]
+})
