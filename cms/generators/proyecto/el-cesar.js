4df6f08f3d24ea2eef7af618845e3c931d2e3eed
Will Alvarez <will.alvarez@gmail.com>
2018-10-01 18:50:50 -0500

    proyecto_parciales

--- cms/generators/proyecto/el-cesar.js
+++ cms/generators/proyecto/el-cesar.js
@@ -1,33 +1,31 @@
 ({
-	titulo_proyecto: 'DOLORES',
+	titulo_proyecto: 'EL CÉSAR',
 	tipo_de_proyecto: 'TV-SERIES',
 	$logo_proyecto_type: 'image',
-	logo_proyecto: '/remote/direct_uploads/1537216963_dolores_logo.svg',
+	logo_proyecto: '',
 	$imagen_homepage_type: 'image',
-	imagen_homepage: '/remote/direct_uploads/1537216955_dolores-80.jpg',
-	$imagen_titulo_type: 'image',
-	imagen_titulo: '',
+	imagen_homepage: '/remote/direct_uploads/1538435633_imagen_titulo.jpeg',
 	$descripcion_corta_proyecto_type: 'textarea',
-	descripcion_corta_proyecto: 'IN A WORLD DOMINATED BY MEN, A TENACIOUS WOMAN WITH AN OUTSPOKEN PERSONALITY MAKES HEADWAY INTO SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SAVVINESS.',
+	descripcion_corta_proyecto: '',
 	$video_fullscreen_type: 'file',
 	video_fullscreen: '',
-	genero_del_proyecto: '',
-	'año': '2017',
+	genero_del_proyecto: 'ACCIÓN',
+	'año': '1',
 	temporadas: '1',
 	episodios: '27',
 	nombre_del_cliente: 'Disney',
 	$logotipo_del_cliente_type: 'image',
-	logotipo_del_cliente: {},
+	logotipo_del_cliente: '/remote/direct_uploads/1538432888_disney-client.svg',
 	$imagen_sinopsis_type: 'image',
-	imagen_sinopsis: '/remote/direct_uploads/1538431714_image_sinopsis.jpeg',
+	imagen_sinopsis: '/remote/direct_uploads/1538432897_image_sinopsis.jpeg',
 	$texto_sinopsis_type: 'textarea',
 	texto_sinopsis: 'EL CÉSAR, es una serie biográfica dramatizada que sigue los pasos de la leyenda de México y del box mundial, Julio César Chávez. Durante 13 años, 11 meses y 14 días, Julio se mantuvo invicto en la cima del fervor popular y lo tuvo todo: familia, fama, dinero a manos llenas y legiones de seguidores. Sin embargo, tan alta es la cima como dura su caída. \n\nAsí, Julio accedió a un privilegiado mundo que lo llevó a relacionarse con peligrosas bandas de narcotraficantes,  a sostener amoríos con afamadas estrellas de televisión, a codearse con las más altas esferas del poder político mexicano y a envolverse en un torbellino de adicciones al alcohol y a las drogas que pondrían fin a su carrera y, por poco, hasta a su vida. \n\nRehabilitado y activo en el medio boxístico, Chávez vuelve con El César, una emocionante serie de 26 capítulos de una hora de duración que sigue la tormentosa vida dentro y fuera del ring del Gran Campeón Mexicano, mientras recorre el difícil camino que lo convertirá en leyenda.',
-	trailer_url: '',
-	ver_proyecto_texto: 'ver proyecto',
+	trailer_url: 'https://www.youtube.com/embed/jtn4121bN_w',
+	ver_proyecto_texto: '/proyecto/el-cesar',
 	imagenes_backstage: [
 		{
 			$imagen_type: 'image',
-			imagen: ''
+			imagen: '/remote/direct_uploads/1538433214_elcesar3.jpg'
 		}
 	],
 	tags: [
@@ -41,7 +39,7 @@
 			nombre_del_tag: 'Script'
 		},
 		{
-			nombre_del_tag: 'Cinematogrphy'
+			nombre_del_tag: 'Cinematography'
 		},
 		{
 			nombre_del_tag: 'Production'
