4f8b8c704884442acb10a707382bdf83f8a7820e
Will Alvarez <will.alvarez@gmail.com>
2018-10-22 17:37:50 -0500

    img2

--- cms/about.js
+++ cms/about.js
@@ -44,109 +44,109 @@
 			nombre: 'Amazon',
 			link: 'http://amazon.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537907916_amazon-client.svg'
+			logo: '/remote/direct_uploads/1540245889_amazon-client.svg'
 		},
 		{
 			nombre: 'Disney',
 			link: 'http://www.disney.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537907967_disney-client.svg'
+			logo: '/remote/direct_uploads/1540247300_disney-client.svg'
 		},
 		{
 			nombre: 'Netflix',
 			link: 'http://www.netflix.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537907999_netflix-client.svg'
+			logo: '/remote/direct_uploads/1540247310_netflix-client.svg'
 		},
 		{
 			nombre: 'Youtube',
 			link: 'www.youtube.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908021_youtube-client.svg'
+			logo: '/remote/direct_uploads/1540247328_youtube-client.svg'
 		},
 		{
 			nombre: 'TNT',
 			link: 'www.tnt.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908046_tnt-client.svg'
+			logo: '/remote/direct_uploads/1540247334_tnt-client.svg'
 		},
 		{
 			nombre: 'Telemundo',
 			link: 'www.telemundo.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908083_telemundo-client.svg'
+			logo: '/remote/direct_uploads/1540247341_telemundo-client.svg'
 		},
 		{
 			nombre: 'Claro',
 			link: 'www.clarovideo.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908135_claro-client.svg'
+			logo: '/remote/direct_uploads/1540247364_claro-client.svg'
 		},
 		{
 			nombre: 'Space',
 			link: 'www.space.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908153_space-client.svg'
+			logo: '/remote/direct_uploads/1540247396_once-client.svg'
 		},
 		{
 			nombre: 'Univision',
 			link: 'www.univision.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908172_univision-client.svg'
+			logo: '/remote/direct_uploads/1540247411_univision-client.svg'
 		},
 		{
 			nombre: 'El Trece',
 			link: 'www.eltrece.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908199_eltrece-client.svg'
+			logo: '/remote/direct_uploads/1540247422_eltrece-client.svg'
 		},
 		{
 			nombre: 'Telefe',
 			link: 'www.telefe.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908221_telefe-client.svg'
+			logo: '/remote/direct_uploads/1540247428_telefe-client.svg'
 		},
 		{
 			nombre: 'RCN',
 			link: 'www.rcn.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908237_rcn-client.svg'
+			logo: '/remote/direct_uploads/1540247435_rcn-client.svg'
 		},
 		{
 			nombre: 'Azteca',
 			link: 'www.tvazteca.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908263_azteca-client.svg'
+			logo: '/remote/direct_uploads/1540247443_azteca-client.svg'
 		},
 		{
 			nombre: 'L',
 			link: 'www.l.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908280_ele-client.svg'
+			logo: '/remote/direct_uploads/1540245939_ele-client.svg'
 		},
 		{
 			nombre: 'Canal Once',
 			link: 'www.oncetv.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908303_once-client.svg'
+			logo: '/remote/direct_uploads/1540247450_once-client.svg'
 		},
 		{
 			nombre: 'Imagen',
 			link: 'www.grupoimagen.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908326_imagen-client.svg'
+			logo: '/remote/direct_uploads/1540247466_imagen-client.svg'
 		},
 		{
 			nombre: 'Conaculta',
 			link: 'www.conaculta.gob',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908346_conaculta-client.svg'
+			logo: '/remote/direct_uploads/1540247471_conaculta-client.svg'
 		},
 		{
 			nombre: 'Chile Vision',
 			link: 'www.chilevision.com',
 			$logo_type: 'image',
-			logo: '/remote/direct_uploads/1537908368_chile-client.svg'
+			logo: '/remote/direct_uploads/1540247476_chile-client.svg'
 		}
 	]
 })
