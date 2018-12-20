c6af87d9d3d78d9a80bbef5f936792eccf62c034
Will Alvarez <will.alvarez@gmail.com>
2018-12-20 14:05:39 -0600

    social links

--- cms/global/global.js
+++ cms/global/global.js
@@ -1,29 +1,29 @@
 ({
-	homepage_ver_proyecto_texto: 'ver proyecto',
-	redes_sociales: [
-		{
-			nombre: 'youtube',
-			link: 'youtube.com'
-		},
-		{
-			nombre: 'instagram',
-			link: 'instagram.com'
-		},
-		{
-			nombre: 'facebook',
-			link: 'facebook.com'
-		}
-	],
-	'$música_del_sitio_type': 'file',
-	'música_del_sitio': '',
-	homepage_videos: [
-		{
-			$video_type: 'file',
-			video: '/remote/direct_uploads/1540248994_inicio.webm'
-		},
-		{
-			$video_type: 'file',
-			video: '/remote/direct_uploads/1544471487_Inicio.mp4'
-		}
-	]
-})
+  homepage_ver_proyecto_texto: "ver proyecto",
+  redes_sociales: [
+    {
+      nombre: "youtube",
+      link: "https://www.youtube.com/channel/UCEbOobSQSyUtnA21a34O07w"
+    },
+    {
+      nombre: "instagram",
+      link: "https://www.instagram.com/btfmedia/?hl=en"
+    },
+    {
+      nombre: "facebook",
+      link: "https://www.facebook.com/btfmediamx/"
+    }
+  ],
+  $música_del_sitio_type: "file",
+  música_del_sitio: "",
+  homepage_videos: [
+    {
+      $video_type: "file",
+      video: "/remote/direct_uploads/1540248994_inicio.webm"
+    },
+    {
+      $video_type: "file",
+      video: "/remote/direct_uploads/1544471487_Inicio.mp4"
+    }
+  ]
+});
