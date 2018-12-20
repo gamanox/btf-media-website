35914943cb80fcfcba8a93858e3a71e156550d8a
willalvarez <will.alvarez@gmail.com>
2018-12-19 20:30:46 -0700

    cesar y selena traducción

--- cms/generators/proyecto/el-cesar.js
+++ cms/generators/proyecto/el-cesar.js
@@ -1,134 +1,215 @@
 ({
-  titulo_proyecto: "El cesar",
-  tagline_proyecto: "",
-  tipo_de_proyecto: "tv",
-  $logo_proyecto_type: "image",
-  logo_proyecto:
-    "/remote/direct_uploads/1545270823_1538513995_elcesar-logo.svg",
-  imdb: "https://www.imdb.com/title/tt6749558/",
-  $imagen_homepage_type: "image",
-  imagen_homepage: "/remote/direct_uploads/1545270788_Home_ElCesar.jpg",
-  $descripcion_corta_proyecto_type: "textarea",
-  descripcion_corta_proyecto:
-    "LA CRÓNICA DE UN PAÍS QUE VIO AL ÍDOLO CAER Y A UN HOMBRE LEVANTARSE.",
-  $video_fullscreen_type: "file",
-  video_fullscreen: "",
-  genero: {
-    nombre: "género",
-    texto: "biodrama político"
-  },
-  formato: {
-    nombre: "formato",
-    texto: "26 x 45'"
-  },
-  idioma: {
-    nombre: "idioma",
-    texto: "español"
-  },
-  episodios: {
-    nombre: "episodios",
-    texto: "26"
-  },
-  cliente: {
-    nombre: "cliente",
-    texto: "disney",
-    $logotipo_del_cliente_type: "image",
-    logotipo_del_cliente:
-      "/remote/direct_uploads/1545269916_1537907967_disney-client.svg"
-  },
-  galeria_sinopsis: [
-    {
-      $imagen_sinopsis_type: "image",
-      imagen_sinopsis: "/remote/direct_uploads/1545271009_El-Cesar_1.jpg",
-      texto_imagen: ""
-    },
-    {
-      $imagen_sinopsis_type: "image",
-      imagen_sinopsis: "/remote/direct_uploads/1545271056_El-Cesar_2.jpg",
-      texto_imagen: ""
-    },
-    {
-      $imagen_sinopsis_type: "image",
-      imagen_sinopsis: "/remote/direct_uploads/1545271066_El-Cesar_3.jpg",
-      texto_imagen: ""
-    },
-    {
-      $imagen_sinopsis_type: "image",
-      imagen_sinopsis: "/remote/direct_uploads/1545271076_El-Cesar_4.jpg",
-      texto_imagen: ""
-    }
-  ],
-  $texto_sinopsis_type: "textarea",
-  texto_sinopsis:
-    "Durante 13 años, 11 mese y 14 días, Julio se mantuvo invicto en la cima del fervor popular y lo tuvo todo: familia, fama, dinero a manos llenas y legiones de seguidores. Sin embargo, tan alta es la cima como dura su caída.\n\nJulio accedió a un privilegiado mundo que lo llevó a relacionarse con peligrosas bandas de narcotraficantes, a sostener amoríos con afamadas estrellas de televisión, a codearse con las más altas esferas del poder político mexicano y a envolverse en un torbellino de adicciones que pondrían fin a su carrera y, por poco, hasta su vida. Rehabilitado y activo en el medio boxístico, Chávez vuelve con “El César”. Una emocionante serie que sigue la tormentosa vida, dentro y fuera del ring, del Gran Campeón Mexicano mientras recorre el difícil camino que lo convertirá en leyenda.",
-  texto_directores: "directores",
-  directores: [
-    {
-      nombre: "Alejandro Aimetta"
-    },
-    {
-      nombre: "Alfonso Pineda Ulloa"
-    }
-  ],
-  texto_actores: "cast principal",
-  actores: [
-    {
-      nombre: "Armando Hernández "
-    },
-    {
-      nombre: "Marcela Guirado"
-    },
-    {
-      nombre: "Julio Bracho"
-    },
-    {
-      nombre: "Leticia Huijara"
-    },
-    {
-      nombre: "Enoc Leaño"
-    },
-    {
-      nombre: "Álvaro Guerrero"
-    },
-    {
-      nombre: "Damayanti Quintanar "
-    },
-    {
-      nombre: "Rocío Verdejo "
-    },
-    {
-      nombre: "Luis Alberti"
-    },
-    {
-      nombre: "Adrián Makala "
-    },
-    {
-      nombre: "María Aura "
-    },
-    {
-      nombre: "Cecilia Suárez"
-    }
-  ],
-  trailer_url: "",
-  ver_proyecto_texto: "",
-  imagenes_backstage: [
-    {
-      $imagen_type: "image",
-      imagen: ""
-    }
-  ],
-  $premios_min: 1,
-  premios: [
-    {
-      titulo: "nominacion killer serie",
-      $logo_type: "image",
-      logo: "/remote/direct_uploads/1545272208_1538514022_cesar-premio.svg",
-      url: ""
-    }
-  ],
-  tags: [
-    {
-      nombre_del_tag: ""
-    }
-  ]
-});
+	titulo_proyecto: 'El cesar',
+	tagline_proyecto: '',
+	tipo_de_proyecto: 'tv',
+	$logo_proyecto_type: 'image',
+	logo_proyecto: '/remote/direct_uploads/1545270823_1538513995_elcesar-logo.svg',
+	imdb: 'https://www.imdb.com/title/tt6749558/',
+	$imagen_homepage_type: 'image',
+	imagen_homepage: '/remote/direct_uploads/1545270788_Home_ElCesar.jpg',
+	$descripcion_corta_proyecto_type: 'textarea',
+	descripcion_corta_proyecto: 'LA CRÓNICA DE UN PAÍS QUE VIO AL ÍDOLO CAER Y A UN HOMBRE LEVANTARSE.',
+	$video_fullscreen_type: 'file',
+	video_fullscreen: '',
+	genero: {
+		nombre: 'género',
+		texto: 'biodrama político',
+		$nombre_en: 'GENRE',
+		$texto_en: 'POLITICAL BIODRAMA'
+	},
+	formato: {
+		nombre: 'formato',
+		texto: '26 x 45\'',
+		$nombre_en: 'FORMAT',
+		$texto_en: '26x45’'
+	},
+	idioma: {
+		nombre: 'idioma',
+		texto: 'español',
+		$nombre_en: 'LANGUAGE',
+		$texto_en: 'SPANISH'
+	},
+	episodios: {
+		nombre: 'episodios',
+		texto: '26',
+		$nombre_en: 'EPISODES',
+		$texto_en: '26'
+	},
+	cliente: {
+		nombre: 'cliente',
+		texto: 'disney',
+		$logotipo_del_cliente_type: 'image',
+		logotipo_del_cliente: '/remote/direct_uploads/1545269916_1537907967_disney-client.svg',
+		$nombre_en: 'CLIENT',
+		$texto_en: 'DISNEY (IMG)'
+	},
+	galeria_sinopsis: [
+		{
+			$imagen_sinopsis_type: 'image',
+			imagen_sinopsis: '/remote/direct_uploads/1545271009_El-Cesar_1.jpg',
+			texto_imagen: ''
+		},
+		{
+			$imagen_sinopsis_type: 'image',
+			imagen_sinopsis: '/remote/direct_uploads/1545271056_El-Cesar_2.jpg',
+			texto_imagen: ''
+		},
+		{
+			$imagen_sinopsis_type: 'image',
+			imagen_sinopsis: '/remote/direct_uploads/1545271066_El-Cesar_3.jpg',
+			texto_imagen: ''
+		},
+		{
+			$imagen_sinopsis_type: 'image',
+			imagen_sinopsis: '/remote/direct_uploads/1545271076_El-Cesar_4.jpg',
+			texto_imagen: ''
+		}
+	],
+	$texto_sinopsis_type: 'textarea',
+	texto_sinopsis: 'Durante 13 años, 11 mese y 14 días, Julio se mantuvo invicto en la cima del fervor popular y lo tuvo todo: familia, fama, dinero a manos llenas y legiones de seguidores. Sin embargo, tan alta es la cima como dura su caída.\n\nJulio accedió a un privilegiado mundo que lo llevó a relacionarse con peligrosas bandas de narcotraficantes, a sostener amoríos con afamadas estrellas de televisión, a codearse con las más altas esferas del poder político mexicano y a envolverse en un torbellino de adicciones que pondrían fin a su carrera y, por poco, hasta su vida. Rehabilitado y activo en el medio boxístico, Chávez vuelve con “El César”. Una emocionante serie que sigue la tormentosa vida, dentro y fuera del ring, del Gran Campeón Mexicano mientras recorre el difícil camino que lo convertirá en leyenda.',
+	texto_directores: 'directores',
+	directores: [
+		{
+			nombre: 'Alejandro Aimetta',
+			$nombre_en: 'Alejandro Aimetta'
+		},
+		{
+			nombre: 'Alfonso Pineda Ulloa',
+			$nombre_en: 'Alfonso Pineda Ulloa'
+		}
+	],
+	texto_actores: 'cast principal',
+	actores: [
+		{
+			nombre: 'Armando Hernández ',
+			$nombre_en: 'Armando Hernández'
+		},
+		{
+			nombre: 'Marcela Guirado',
+			$nombre_en: 'Marcela Guirado'
+		},
+		{
+			nombre: 'Julio Bracho',
+			$nombre_en: 'Julio Bracho'
+		},
+		{
+			nombre: 'Leticia Huijara',
+			$nombre_en: 'Leticia Huijara'
+		},
+		{
+			nombre: 'Enoc Leaño',
+			$nombre_en: 'Enoc Leaño'
+		},
+		{
+			nombre: 'Álvaro Guerrero',
+			$nombre_en: 'Álvaro Guerrero'
+		},
+		{
+			nombre: 'Damayanti Quintanar ',
+			$nombre_en: 'Damayanti Quintanar'
+		},
+		{
+			nombre: 'Rocío Verdejo ',
+			$nombre_en: 'Rocío Verdejo'
+		},
+		{
+			nombre: 'Luis Alberti',
+			$nombre_en: 'Luis Alberti'
+		},
+		{
+			nombre: 'Adrián Makala ',
+			$nombre_en: 'Adrián Makala'
+		},
+		{
+			nombre: 'María Aura ',
+			$nombre_en: 'María Aura'
+		},
+		{
+			nombre: 'Cecilia Suárez',
+			$nombre_en: 'Cecilia Suárez'
+		}
+	],
+	trailer_url: '',
+	ver_proyecto_texto: '',
+	imagenes_backstage: [
+		{
+			$imagen_type: 'image',
+			imagen: ''
+		}
+	],
+	$premios_min: 1,
+	premios: [
+		{
+			titulo: 'nominacion killer serie',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1545272208_1538514022_cesar-premio.svg',
+			url: ''
+		}
+	],
+	tags: [
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Direction'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Develop'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Script'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Cinematography'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Production'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Music'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Casting'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Editing'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Costume Design'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Art'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Makeup'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Photography'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Animation'
+		},
+		{
+			nombre_del_tag: '',
+			$nombre_del_tag_en: 'Brand Id'
+		}
+	],
+	$titulo_proyecto_en: 'The cesar',
+	$tipo_de_proyecto_en: 'tv',
+	$imdb_en: 'https://www.imdb.com/title/tt6749558/',
+	$descripcion_corta_proyecto_en: 'THE CHRONICLE OF A COUNTRY THAT SAW THE IDOL FALL AND A MAN LIFTING UP.',
+	$texto_sinopsis_en: 'For 13 years, 11 months and 14 days, Julio remained unbeaten at the peak of popular fervor and had it all: family, fame, money to hands and legions of followers. However, the top is as high as its fall.\n\nJulio agreed to a privileged world that led him to engage with dangerous drug gangs, to maintain love affairs with famous television stars, to rub shoulders with the highest spheres of Mexican political power and to engage in a whirlwind of addictions that would end his career and, almost, until his life. Rehabilitated and active in boxing, Chávez returns with "El César". An exciting series that follows the stormy life, inside and outside the ring, of the Great Mexican Champion as he travels the difficult path that will turn him into a legend.',
+	$texto_directores_en: 'DIRECTORS',
+	$texto_actores_en: 'MAIN CAST',
+	$ver_proyecto_texto_en: 'see proyect'
+})
