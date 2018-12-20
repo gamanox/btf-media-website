75d78355f89300bea1b2ea996c33eed2d44e1d2d
Will Alvarez <will.alvarez@gmail.com>
2018-12-20 14:02:41 -0600

    trailers

--- cms/generators/proyecto/dolores.js
+++ cms/generators/proyecto/dolores.js
@@ -1,225 +1,232 @@
 ({
-	titulo_proyecto: 'SU NOMBRE ERA DOLORES, LA JENN QUE YO CONOCÍ',
-	tagline_proyecto: '',
-	tipo_de_proyecto: 'tv',
-	$logo_proyecto_type: 'image',
-	logo_proyecto: '/remote/direct_uploads/1545272572_1537216963_dolores_logo.svg',
-	imdb: 'https://www.imdb.com/title/tt6431852/',
-	$imagen_homepage_type: 'image',
-	imagen_homepage: '/remote/direct_uploads/1545272588_Home_Dolores.jpg',
-	$descripcion_corta_proyecto_type: 'textarea',
-	descripcion_corta_proyecto: 'IN A WORLD DOMINATED BY MEN, A TENACIOUS WOMAN WITH AN OUTSPOKEN PERSONALITY MAKES HEADWAY INTO SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SAVVINESS.',
-	$video_fullscreen_type: 'file',
-	video_fullscreen: '',
-	genero: {
-		nombre: 'género',
-		texto: 'biodrama',
-		$nombre_en: 'GENRE',
-		$texto_en: 'biopic'
-	},
-	formato: {
-		nombre: 'formato',
-		texto: '26 x 46\'',
-		$nombre_en: 'format',
-		$texto_en: '26x46’'
-	},
-	idioma: {
-		nombre: 'idioma',
-		texto: 'español-inglés',
-		$nombre_en: 'lenguage',
-		$texto_en: 'SPANISH-ENGLISH'
-	},
-	episodios: {
-		nombre: 'episodios',
-		texto: '26',
-		$nombre_en: 'episodes',
-		$texto_en: '26'
-	},
-	cliente: {
-		nombre: 'cliente',
-		texto: '',
-		$logotipo_del_cliente_type: 'image',
-		logotipo_del_cliente: '',
-		$nombre_en: 'client'
-	},
-	galeria_sinopsis: [
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545272984_Dolores_1.jpg',
-			texto_imagen: ''
-		},
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545272990_Dolores_2.jpg',
-			texto_imagen: ''
-		},
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545273001_Dolores_3.jpg',
-			texto_imagen: ''
-		}
-	],
-	$texto_sinopsis_type: 'textarea',
-	texto_sinopsis: 'El mundo se sorprende ante su llegada: mientras unos la elevan a la categoría de Diva, otros intentan frenar su ascenso. Lo cierto es que su fama es un misil que nadie puede detener, un lujo que compensa todo previo sinsabor y su llave para convertirse en la artista latina más importante del momento. Pero, sobre todo un arma lista para derribar sus ambiciones al instante en el que no pueda mantener la guardia arriba.\n\n“Dolores” es la historia de la perfectamente imperfecta madre y padre cinco hijos, la mariposa de barrio que un día se vuelve La Diva de la Banda, la vida de la incansable Jenni Rivera.',
-	texto_directores: 'directores',
-	directores: [
-		{
-			nombre: 'Alfonso Pineda Ulloa ',
-			$nombre_en: 'Alfonso Pineda Ulloa'
-		},
-		{
-			nombre: 'Álvaro Curiel',
-			$nombre_en: 'Álvaro Curiel'
-		},
-		{
-			nombre: 'Alejandro Aimetta',
-			$nombre_en: 'Alejandro Aimetta'
-		}
-	],
-	texto_actores: 'cast principal',
-	actores: [
-		{
-			nombre: 'Luz Ramos ',
-			$nombre_en: 'Luz Ramos'
-		},
-		{
-			nombre: 'Javier Díaz Dueñas ',
-			$nombre_en: 'Javier Díaz Dueñas'
-		},
-		{
-			nombre: 'Luis Felipe Tovar ',
-			$nombre_en: 'Luis Felipe Tovar'
-		},
-		{
-			nombre: 'Geraldine Galván ',
-			$nombre_en: 'Geraldine Galván'
-		},
-		{
-			nombre: 'María Rojo',
-			$nombre_en: 'María Rojo'
-		},
-		{
-			nombre: 'Rubén Zamora ',
-			$nombre_en: 'Rubén Zamora'
-		},
-		{
-			nombre: 'Fernando Alonso',
-			$nombre_en: 'Fernando Alonso'
-		},
-		{
-			nombre: 'Juan Soler',
-			$nombre_en: 'Juan Soler'
-		},
-		{
-			nombre: 'Luis Felipe Tovar',
-			$nombre_en: 'Luis Felipe Tovar'
-		},
-		{
-			nombre: 'Lumi Cavazos ',
-			$nombre_en: 'Lumi Cavazos'
-		},
-		{
-			nombre: 'Regina Orozco ',
-			$nombre_en: 'Regina Orozco'
-		},
-		{
-			nombre: 'Álex Perea',
-			$nombre_en: 'Álex Perea'
-		},
-		{
-			nombre: 'Daniel Martínez',
-			$nombre_en: 'Daniel Martínez'
-		},
-		{
-			nombre: 'Claudette Maillé',
-			$nombre_en: 'Claudette Maillé'
-		},
-		{
-			nombre: 'Dagoberto Gama',
-			$nombre_en: 'Dagoberto Gama'
-		}
-	],
-	trailer_url: '',
-	ver_proyecto_texto: 'ver proyecto',
-	imagenes_backstage: [
-		{
-			$imagen_type: 'image',
-			imagen: ''
-		}
-	],
-	$premios_min: 1,
-	premios: [
-		{
-			titulo: '',
-			$logo_type: 'image',
-			logo: '',
-			url: ''
-		}
-	],
-	tags: [
-		{
-			nombre_del_tag: 'Dirección',
-			$nombre_del_tag_en: 'Direction'
-		},
-		{
-			nombre_del_tag: 'Desarrollo',
-			$nombre_del_tag_en: 'Develop'
-		},
-		{
-			nombre_del_tag: 'Guión',
-			$nombre_del_tag_en: 'Script'
-		},
-		{
-			nombre_del_tag: 'Cinematografía',
-			$nombre_del_tag_en: 'Cinematography'
-		},
-		{
-			nombre_del_tag: 'Producción',
-			$nombre_del_tag_en: 'Production'
-		},
-		{
-			nombre_del_tag: 'Música',
-			$nombre_del_tag_en: 'Music'
-		},
-		{
-			nombre_del_tag: 'Casting',
-			$nombre_del_tag_en: 'Casting'
-		},
-		{
-			nombre_del_tag: 'Edición',
-			$nombre_del_tag_en: 'Editing'
-		},
-		{
-			nombre_del_tag: 'Diseño de vestuario',
-			$nombre_del_tag_en: 'Costume Design'
-		},
-		{
-			nombre_del_tag: 'Arte',
-			$nombre_del_tag_en: 'Art'
-		},
-		{
-			nombre_del_tag: 'Maquillaje',
-			$nombre_del_tag_en: 'Makeup'
-		},
-		{
-			nombre_del_tag: 'Fotografía',
-			$nombre_del_tag_en: 'Photography'
-		},
-		{
-			nombre_del_tag: 'Animación',
-			$nombre_del_tag_en: 'Animation'
-		},
-		{
-			nombre_del_tag: 'Identificación de la marca',
-			$nombre_del_tag_en: 'Brand Id'
-		}
-	],
-	$titulo_proyecto_en: 'HIS NAME WAS DOLORES, THE JENN THAT I KNOWN',
-	$tipo_de_proyecto_en: 'tv',
-	$imdb_en: 'https://www.imdb.com/title/tt6431852/',
-	$descripcion_corta_proyecto_en: 'IN A WORLD DOMINATED BY MEN, A WOMAN WITH EXTENDED PERSONALITY STARTS SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SURVEILLANCE.',
-	$texto_sinopsis_en: 'The world is surprised at his arrival: while some raise it to the category of Diva, others try to stop his rise. The truth is that her fame is a missile that no one can stop, a luxury that compensates for all previous discontent and its key to become the most important Latin artist of the moment. But, above all, a weapon ready to knock down your ambitions instantly when you can not keep your guard up.\n\n"Dolores" is the story of the perfectly imperfect mother and father of five children, the neighborhood butterfly that one day becomes La Diva de la Banda, the life of the tireless Jenni Rivera.',
-	$texto_directores_en: 'DIRECTORS',
-	$texto_actores_en: 'MAIN CAST',
-	$ver_proyecto_texto_en: 'see proyect'
-})
+  titulo_proyecto: "SU NOMBRE ERA DOLORES, LA JENN QUE YO CONOCÍ",
+  tagline_proyecto: "",
+  tipo_de_proyecto: "tv",
+  $logo_proyecto_type: "image",
+  logo_proyecto:
+    "/remote/direct_uploads/1545272572_1537216963_dolores_logo.svg",
+  imdb: "https://www.imdb.com/title/tt6431852/",
+  $imagen_homepage_type: "image",
+  imagen_homepage: "/remote/direct_uploads/1545272588_Home_Dolores.jpg",
+  $descripcion_corta_proyecto_type: "textarea",
+  descripcion_corta_proyecto:
+    "IN A WORLD DOMINATED BY MEN, A TENACIOUS WOMAN WITH AN OUTSPOKEN PERSONALITY MAKES HEADWAY INTO SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SAVVINESS.",
+  $video_fullscreen_type: "file",
+  video_fullscreen: "",
+  genero: {
+    nombre: "género",
+    texto: "biodrama",
+    $nombre_en: "GENRE",
+    $texto_en: "biopic"
+  },
+  formato: {
+    nombre: "formato",
+    texto: "26 x 46'",
+    $nombre_en: "format",
+    $texto_en: "26x46’"
+  },
+  idioma: {
+    nombre: "idioma",
+    texto: "español-inglés",
+    $nombre_en: "lenguage",
+    $texto_en: "SPANISH-ENGLISH"
+  },
+  episodios: {
+    nombre: "episodios",
+    texto: "26",
+    $nombre_en: "episodes",
+    $texto_en: "26"
+  },
+  cliente: {
+    nombre: "cliente",
+    texto: "",
+    $logotipo_del_cliente_type: "image",
+    logotipo_del_cliente: "",
+    $nombre_en: "client"
+  },
+  galeria_sinopsis: [
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545272984_Dolores_1.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545272990_Dolores_2.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545273001_Dolores_3.jpg",
+      texto_imagen: ""
+    }
+  ],
+  $texto_sinopsis_type: "textarea",
+  texto_sinopsis:
+    "El mundo se sorprende ante su llegada: mientras unos la elevan a la categoría de Diva, otros intentan frenar su ascenso. Lo cierto es que su fama es un misil que nadie puede detener, un lujo que compensa todo previo sinsabor y su llave para convertirse en la artista latina más importante del momento. Pero, sobre todo un arma lista para derribar sus ambiciones al instante en el que no pueda mantener la guardia arriba.\n\n“Dolores” es la historia de la perfectamente imperfecta madre y padre cinco hijos, la mariposa de barrio que un día se vuelve La Diva de la Banda, la vida de la incansable Jenni Rivera.",
+  texto_directores: "directores",
+  directores: [
+    {
+      nombre: "Alfonso Pineda Ulloa ",
+      $nombre_en: "Alfonso Pineda Ulloa"
+    },
+    {
+      nombre: "Álvaro Curiel",
+      $nombre_en: "Álvaro Curiel"
+    },
+    {
+      nombre: "Alejandro Aimetta",
+      $nombre_en: "Alejandro Aimetta"
+    }
+  ],
+  texto_actores: "cast principal",
+  actores: [
+    {
+      nombre: "Luz Ramos ",
+      $nombre_en: "Luz Ramos"
+    },
+    {
+      nombre: "Javier Díaz Dueñas ",
+      $nombre_en: "Javier Díaz Dueñas"
+    },
+    {
+      nombre: "Luis Felipe Tovar ",
+      $nombre_en: "Luis Felipe Tovar"
+    },
+    {
+      nombre: "Geraldine Galván ",
+      $nombre_en: "Geraldine Galván"
+    },
+    {
+      nombre: "María Rojo",
+      $nombre_en: "María Rojo"
+    },
+    {
+      nombre: "Rubén Zamora ",
+      $nombre_en: "Rubén Zamora"
+    },
+    {
+      nombre: "Fernando Alonso",
+      $nombre_en: "Fernando Alonso"
+    },
+    {
+      nombre: "Juan Soler",
+      $nombre_en: "Juan Soler"
+    },
+    {
+      nombre: "Luis Felipe Tovar",
+      $nombre_en: "Luis Felipe Tovar"
+    },
+    {
+      nombre: "Lumi Cavazos ",
+      $nombre_en: "Lumi Cavazos"
+    },
+    {
+      nombre: "Regina Orozco ",
+      $nombre_en: "Regina Orozco"
+    },
+    {
+      nombre: "Álex Perea",
+      $nombre_en: "Álex Perea"
+    },
+    {
+      nombre: "Daniel Martínez",
+      $nombre_en: "Daniel Martínez"
+    },
+    {
+      nombre: "Claudette Maillé",
+      $nombre_en: "Claudette Maillé"
+    },
+    {
+      nombre: "Dagoberto Gama",
+      $nombre_en: "Dagoberto Gama"
+    }
+  ],
+  trailer_url: "https://www.youtube.com/embed/KCMYMafPO74",
+  ver_proyecto_texto: "ver proyecto",
+  imagenes_backstage: [
+    {
+      $imagen_type: "image",
+      imagen: ""
+    }
+  ],
+  $premios_min: 1,
+  premios: [
+    {
+      titulo: "",
+      $logo_type: "image",
+      logo: "",
+      url: ""
+    }
+  ],
+  tags: [
+    {
+      nombre_del_tag: "Dirección",
+      $nombre_del_tag_en: "Direction"
+    },
+    {
+      nombre_del_tag: "Desarrollo",
+      $nombre_del_tag_en: "Develop"
+    },
+    {
+      nombre_del_tag: "Guión",
+      $nombre_del_tag_en: "Script"
+    },
+    {
+      nombre_del_tag: "Cinematografía",
+      $nombre_del_tag_en: "Cinematography"
+    },
+    {
+      nombre_del_tag: "Producción",
+      $nombre_del_tag_en: "Production"
+    },
+    {
+      nombre_del_tag: "Música",
+      $nombre_del_tag_en: "Music"
+    },
+    {
+      nombre_del_tag: "Casting",
+      $nombre_del_tag_en: "Casting"
+    },
+    {
+      nombre_del_tag: "Edición",
+      $nombre_del_tag_en: "Editing"
+    },
+    {
+      nombre_del_tag: "Diseño de vestuario",
+      $nombre_del_tag_en: "Costume Design"
+    },
+    {
+      nombre_del_tag: "Arte",
+      $nombre_del_tag_en: "Art"
+    },
+    {
+      nombre_del_tag: "Maquillaje",
+      $nombre_del_tag_en: "Makeup"
+    },
+    {
+      nombre_del_tag: "Fotografía",
+      $nombre_del_tag_en: "Photography"
+    },
+    {
+      nombre_del_tag: "Animación",
+      $nombre_del_tag_en: "Animation"
+    },
+    {
+      nombre_del_tag: "Identificación de la marca",
+      $nombre_del_tag_en: "Brand Id"
+    }
+  ],
+  $titulo_proyecto_en: "HIS NAME WAS DOLORES, THE JENN THAT I KNOWN",
+  $tipo_de_proyecto_en: "tv",
+  $imdb_en: "https://www.imdb.com/title/tt6431852/",
+  $descripcion_corta_proyecto_en:
+    "IN A WORLD DOMINATED BY MEN, A WOMAN WITH EXTENDED PERSONALITY STARTS SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SURVEILLANCE.",
+  $texto_sinopsis_en:
+    'The world is surprised at his arrival: while some raise it to the category of Diva, others try to stop his rise. The truth is that her fame is a missile that no one can stop, a luxury that compensates for all previous discontent and its key to become the most important Latin artist of the moment. But, above all, a weapon ready to knock down your ambitions instantly when you can not keep your guard up.\n\n"Dolores" is the story of the perfectly imperfect mother and father of five children, the neighborhood butterfly that one day becomes La Diva de la Banda, the life of the tireless Jenni Rivera.',
+  $texto_directores_en: "DIRECTORS",
+  $texto_actores_en: "MAIN CAST",
+  $ver_proyecto_texto_en: "see proyect",
+  $trailer_url_en:
+    "https://www.youtube.com/watch?v=KCMYMafPO74&index=2&list=PLAZ8GDUS21w8dUO_3RiKzhq4qygBRtX2F"
+});
