7a0ee23260d91528e4035f030325013dcf023877
Will Alvarez <will.alvarez@gmail.com>
2018-12-19 20:32:36 -0600

    cesar y dolores

---
+++ cms/generators/proyecto/dolores.js
@@ -0,0 +1,140 @@
+({
+  titulo_proyecto: "SU NOMBRE ERA DOLORES, LA JENN QUE YO CONOCÍ",
+  tagline_proyecto: "",
+  tipo_de_proyecto: "tv",
+  $logo_proyecto_type: "image",
+  logo_proyecto:
+    "/remote/direct_uploads/1545272572_1537216963_dolores_logo.svg",
+  imdb: "https://www.imdb.com/title/tt6431852/",
+  $imagen_homepage_type: "image",
+  imagen_homepage: "/remote/direct_uploads/1545272588_Home_Dolores.jpg",
+  $descripcion_corta_proyecto_type: "textarea",
+  descripcion_corta_proyecto:
+    "IN A WORLD DOMINATED BY MEN, A TENACIOUS WOMAN WITH AN OUTSPOKEN PERSONALITY MAKES HEADWAY INTO SUCCESS THROUGH THE USE OF DETERMINATION, PERSEVERANCE AND SAVVINESS.",
+  $video_fullscreen_type: "file",
+  video_fullscreen: "",
+  genero: {
+    nombre: "género",
+    texto: "biodrama"
+  },
+  formato: {
+    nombre: "formato",
+    texto: "26 x 46'"
+  },
+  idioma: {
+    nombre: "idioma",
+    texto: "español-inglés"
+  },
+  episodios: {
+    nombre: "episodios",
+    texto: "26"
+  },
+  cliente: {
+    nombre: "cliente",
+    texto: "",
+    $logotipo_del_cliente_type: "image",
+    logotipo_del_cliente: ""
+  },
+  galeria_sinopsis: [
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545272984_Dolores_1.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545272990_Dolores_2.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545273001_Dolores_3.jpg",
+      texto_imagen: ""
+    }
+  ],
+  $texto_sinopsis_type: "textarea",
+  texto_sinopsis:
+    "El mundo se sorprende ante su llegada: mientras unos la elevan a la categoría de Diva, otros intentan frenar su ascenso. Lo cierto es que su fama es un misil que nadie puede detener, un lujo que compensa todo previo sinsabor y su llave para convertirse en la artista latina más importante del momento. Pero, sobre todo un arma lista para derribar sus ambiciones al instante en el que no pueda mantener la guardia arriba.\n\n“Dolores” es la historia de la perfectamente imperfecta madre y padre cinco hijos, la mariposa de barrio que un día se vuelve La Diva de la Banda, la vida de la incansable Jenni Rivera.",
+  texto_directores: "directores",
+  directores: [
+    {
+      nombre: "Alfonso Pineda Ulloa "
+    },
+    {
+      nombre: "Álvaro Curiel"
+    },
+    {
+      nombre: "Alejandro Aimetta"
+    }
+  ],
+  texto_actores: "cast principal",
+  actores: [
+    {
+      nombre: "Luz Ramos "
+    },
+    {
+      nombre: "Javier Díaz Dueñas "
+    },
+    {
+      nombre: "Luis Felipe Tovar "
+    },
+    {
+      nombre: "Geraldine Galván "
+    },
+    {
+      nombre: "María Rojo"
+    },
+    {
+      nombre: "Rubén Zamora "
+    },
+    {
+      nombre: "Fernando Alonso"
+    },
+    {
+      nombre: "Juan Soler"
+    },
+    {
+      nombre: "Luis Felipe Tovar"
+    },
+    {
+      nombre: "Lumi Cavazos "
+    },
+    {
+      nombre: "Regina Orozco "
+    },
+    {
+      nombre: "Álex Perea"
+    },
+    {
+      nombre: "Daniel Martínez"
+    },
+    {
+      nombre: "Claudette Maillé"
+    },
+    {
+      nombre: "Dagoberto Gama"
+    }
+  ],
+  trailer_url: "",
+  ver_proyecto_texto: "ver proyecto",
+  imagenes_backstage: [
+    {
+      $imagen_type: "image",
+      imagen: ""
+    }
+  ],
+  $premios_min: 1,
+  premios: [
+    {
+      titulo: "",
+      $logo_type: "image",
+      logo: "",
+      url: ""
+    }
+  ],
+  tags: [
+    {
+      nombre_del_tag: ""
+    }
+  ]
+});
--- cms/generators/proyecto/el-cesar.js
+++ cms/generators/proyecto/el-cesar.js
@@ -1,94 +1,134 @@
 ({
-	titulo_proyecto: 'El cesar',
-	tagline_proyecto: '',
-	tipo_de_proyecto: 'tv',
-	$logo_proyecto_type: 'image',
-	logo_proyecto: '/remote/direct_uploads/1545270823_1538513995_elcesar-logo.svg',
-	imdb: '',
-	$imagen_homepage_type: 'image',
-	imagen_homepage: '/remote/direct_uploads/1545270788_Home_ElCesar.jpg',
-	$descripcion_corta_proyecto_type: 'textarea',
-	descripcion_corta_proyecto: 'LA CRÓNICA DE UN PAÍS QUE VIO AL ÍDOLO CAER Y A UN HOMBRE LEVANTARSE.',
-	$video_fullscreen_type: 'file',
-	video_fullscreen: '',
-	genero: {
-		nombre: 'género',
-		texto: 'biodrama político'
-	},
-	formato: {
-		nombre: 'formato',
-		texto: '26 x 45\''
-	},
-	idioma: {
-		nombre: 'idioma',
-		texto: 'español'
-	},
-	episodios: {
-		nombre: 'episodios',
-		texto: '26'
-	},
-	cliente: {
-		nombre: 'cliente',
-		texto: 'disney',
-		$logotipo_del_cliente_type: 'image',
-		logotipo_del_cliente: '/remote/direct_uploads/1545269916_1537907967_disney-client.svg'
-	},
-	galeria_sinopsis: [
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545271009_El-Cesar_1.jpg',
-			texto_imagen: ''
-		},
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545271056_El-Cesar_2.jpg',
-			texto_imagen: ''
-		},
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545271066_El-Cesar_3.jpg',
-			texto_imagen: ''
-		},
-		{
-			$imagen_sinopsis_type: 'image',
-			imagen_sinopsis: '/remote/direct_uploads/1545271076_El-Cesar_4.jpg',
-			texto_imagen: ''
-		}
-	],
-	$texto_sinopsis_type: 'textarea',
-	texto_sinopsis: '',
-	texto_directores: '',
-	directores: [
-		{
-			nombre: ''
-		}
-	],
-	texto_actores: '',
-	actores: [
-		{
-			nombre: ''
-		}
-	],
-	trailer_url: '',
-	ver_proyecto_texto: '',
-	imagenes_backstage: [
-		{
-			$imagen_type: 'image',
-			imagen: ''
-		}
-	],
-	$premios_min: 1,
-	premios: [
-		{
-			titulo: '',
-			$logo_type: 'image',
-			logo: '',
-			url: ''
-		}
-	],
-	tags: [
-		{
-			nombre_del_tag: ''
-		}
-	]
-})
+  titulo_proyecto: "El cesar",
+  tagline_proyecto: "",
+  tipo_de_proyecto: "tv",
+  $logo_proyecto_type: "image",
+  logo_proyecto:
+    "/remote/direct_uploads/1545270823_1538513995_elcesar-logo.svg",
+  imdb: "https://www.imdb.com/title/tt6749558/",
+  $imagen_homepage_type: "image",
+  imagen_homepage: "/remote/direct_uploads/1545270788_Home_ElCesar.jpg",
+  $descripcion_corta_proyecto_type: "textarea",
+  descripcion_corta_proyecto:
+    "LA CRÓNICA DE UN PAÍS QUE VIO AL ÍDOLO CAER Y A UN HOMBRE LEVANTARSE.",
+  $video_fullscreen_type: "file",
+  video_fullscreen: "",
+  genero: {
+    nombre: "género",
+    texto: "biodrama político"
+  },
+  formato: {
+    nombre: "formato",
+    texto: "26 x 45'"
+  },
+  idioma: {
+    nombre: "idioma",
+    texto: "español"
+  },
+  episodios: {
+    nombre: "episodios",
+    texto: "26"
+  },
+  cliente: {
+    nombre: "cliente",
+    texto: "disney",
+    $logotipo_del_cliente_type: "image",
+    logotipo_del_cliente:
+      "/remote/direct_uploads/1545269916_1537907967_disney-client.svg"
+  },
+  galeria_sinopsis: [
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545271009_El-Cesar_1.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545271056_El-Cesar_2.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545271066_El-Cesar_3.jpg",
+      texto_imagen: ""
+    },
+    {
+      $imagen_sinopsis_type: "image",
+      imagen_sinopsis: "/remote/direct_uploads/1545271076_El-Cesar_4.jpg",
+      texto_imagen: ""
+    }
+  ],
+  $texto_sinopsis_type: "textarea",
+  texto_sinopsis:
+    "Durante 13 años, 11 mese y 14 días, Julio se mantuvo invicto en la cima del fervor popular y lo tuvo todo: familia, fama, dinero a manos llenas y legiones de seguidores. Sin embargo, tan alta es la cima como dura su caída.\n\nJulio accedió a un privilegiado mundo que lo llevó a relacionarse con peligrosas bandas de narcotraficantes, a sostener amoríos con afamadas estrellas de televisión, a codearse con las más altas esferas del poder político mexicano y a envolverse en un torbellino de adicciones que pondrían fin a su carrera y, por poco, hasta su vida. Rehabilitado y activo en el medio boxístico, Chávez vuelve con “El César”. Una emocionante serie que sigue la tormentosa vida, dentro y fuera del ring, del Gran Campeón Mexicano mientras recorre el difícil camino que lo convertirá en leyenda.",
+  texto_directores: "directores",
+  directores: [
+    {
+      nombre: "Alejandro Aimetta"
+    },
+    {
+      nombre: "Alfonso Pineda Ulloa"
+    }
+  ],
+  texto_actores: "cast principal",
+  actores: [
+    {
+      nombre: "Armando Hernández "
+    },
+    {
+      nombre: "Marcela Guirado"
+    },
+    {
+      nombre: "Julio Bracho"
+    },
+    {
+      nombre: "Leticia Huijara"
+    },
+    {
+      nombre: "Enoc Leaño"
+    },
+    {
+      nombre: "Álvaro Guerrero"
+    },
+    {
+      nombre: "Damayanti Quintanar "
+    },
+    {
+      nombre: "Rocío Verdejo "
+    },
+    {
+      nombre: "Luis Alberti"
+    },
+    {
+      nombre: "Adrián Makala "
+    },
+    {
+      nombre: "María Aura "
+    },
+    {
+      nombre: "Cecilia Suárez"
+    }
+  ],
+  trailer_url: "",
+  ver_proyecto_texto: "",
+  imagenes_backstage: [
+    {
+      $imagen_type: "image",
+      imagen: ""
+    }
+  ],
+  $premios_min: 1,
+  premios: [
+    {
+      titulo: "nominacion killer serie",
+      $logo_type: "image",
+      logo: "/remote/direct_uploads/1545272208_1538514022_cesar-premio.svg",
+      url: ""
+    }
+  ],
+  tags: [
+    {
+      nombre_del_tag: ""
+    }
+  ]
+});
