45e535bc8d56543a7d66a75d94c61c5328d6ff48
Will Alvarez <will.alvarez@gmail.com>
2018-12-22 17:42:28 -0600

    premios

--- cms/generators/proyecto/Proyecto-4.js
+++ cms/generators/proyecto/Proyecto-4.js
@@ -1,229 +1,225 @@
 ({
-  id: '4',
-  titulo_proyecto: "HASTA QUE TE CONOCÍ",
-  tagline_proyecto: "",
-  tipo_de_proyecto: "tv",
-  $logo_proyecto_type: "image",
-  logo_proyecto: "/remote/direct_uploads/1545279268_1537216060_hqtc_logo.png",
-  imdb: "https://www.imdb.com/title/tt5163162/",
-  $imagen_homepage_type: "image",
-  imagen_homepage: "/remote/direct_uploads/1545279291_Home_HQTC.jpg",
-  $descripcion_corta_proyecto_type: "textarea",
-  descripcion_corta_proyecto:
-    "LA HISTORIA DE UN SER EXTRAORDINARIO QUE TRANSFORMÓ SU DESTINO PARA CONVERTIRSE EN EL CANTAUTOR MÁS QUERIDO DE IBEROAMÉRICA.",
-  $video_fullscreen_type: "file",
-  video_fullscreen: "",
-  genero: {
-    nombre: "género",
-    texto: "biodrama",
-    $nombre_en: "genre",
-    $texto_en: "biopic"
-  },
-  formato: {
-    nombre: "formato",
-    texto: "13 x 45'",
-    $nombre_en: "format",
-    $texto_en: "13x45’"
-  },
-  idioma: {
-    nombre: "idioma",
-    texto: "español",
-    $nombre_en: "lenguage",
-    $texto_en: "spanish"
-  },
-  episodios: {
-    nombre: "episodios",
-    texto: "13",
-    $nombre_en: "episodes",
-    $texto_en: "13"
-  },
-  cliente: {
-    nombre: "cliente",
-    texto: "disney",
-    $logotipo_del_cliente_type: "image",
-    logotipo_del_cliente: "",
-    $nombre_en: "client",
-    $texto_en: "DISNEY (IMG)"
-  },
-  galeria_sinopsis: [
-    {
-      $imagen_sinopsis_type: "image",
-      imagen_sinopsis: "/remote/direct_uploads/1545279357_HQTC_1.jpg",
-      texto_imagen: ""
-    }
-  ],
-  $texto_sinopsis_type: "textarea",
-  texto_sinopsis:
-    "“Hasta que te conocí” una serie que sigue los pasos de Alberto Aguilera Valadéz hacia un sueño de fama. Un recorrido de 13 episodios autobiográficos basados en testimonios inéditos que develan como nunca antes, la vida del Divo de Juárez: Juan Gabriel. Una historia que narra cómo su extraordinario talento llevó a este cantautor a desafiar su destino y superar la pobreza, traiciones y prejuicios hasta convertirse en “Juanga”, el ícono musical más querido de Iberoamérica.",
-  texto_directores: "directores",
-  directores: [
-    {
-      nombre: "Álvaro Curiel",
-      $nombre_en: "Álvaro Curiel"
-    },
-    {
-      nombre: "Rigoberto Castañeda ",
-      $nombre_en: "Rigoberto Castañeda"
-    },
-    {
-      nombre: "Alfonso Pineda Ulloa",
-      $nombre_en: "Alfonso Pineda Ulloa"
-    },
-    {
-      nombre: "Alejandro Aimetta",
-      $nombre_en: "Alejandro Aimetta"
-    }
-  ],
-  texto_actores: "cast principal",
-  actores: [
-    {
-      nombre: "Julián Román",
-      $nombre_en: "Julián Román"
-    },
-    {
-      nombre: "Dolores Heredia ",
-      $nombre_en: "Dolores Heredia "
-    },
-    {
-      nombre: "Marco Treviño",
-      $nombre_en: "Marco Treviño"
-    },
-    {
-      nombre: "Irán Castillo",
-      $nombre_en: "Irán Castillo"
-    },
-    {
-      nombre: "Verónica Merchant ",
-      $nombre_en: "Verónica Merchant "
-    },
-    {
-      nombre: "Ernesto Gómez Cruz ",
-      $nombre_en: "Ernesto Gómez Cruz "
-    },
-    {
-      nombre: "Isabel Burr",
-      $nombre_en: "Isabel Burr"
-    },
-    {
-      nombre: "Carlos Elias Yorvick",
-      $nombre_en: "Carlos Elias Yorvick"
-    },
-    {
-      nombre: "Damayanti Quintanar",
-      $nombre_en: "Damayanti Quintanar"
-    },
-    {
-      nombre: "Verónica Langer",
-      $nombre_en: "Verónica Langer"
-    },
-    {
-      nombre: "Luz Treviño",
-      $nombre_en: "Luz Treviño"
-    },
-    {
-      nombre: "Luz Ramos ",
-      $nombre_en: "Luz Ramos "
-    },
-    {
-      nombre: "Geraldine Galván ",
-      $nombre_en: "Geraldine Galván "
-    },
-    {
-      nombre: "Alejandro Calva ",
-      $nombre_en: "Alejandro Calva "
-    },
-    {
-      nombre: "Tenoch Huerta ",
-      $nombre_en: "Tenoch Huerta "
-    },
-    {
-      nombre: "Andrés Palacios",
-      $nombre_en: "Andrés Palacios"
-    }
-  ],
-  trailer_url: "https://www.youtube.com/embed/y1ITX16k-qs",
-  ver_proyecto_texto: "ver proyecto",
-  imagenes_backstage: [
-    {
-      $imagen_type: "image",
-      imagen: ""
-    }
-  ],
-  $premios_min: 1,
-  premios: [
-    {
-      titulo: "",
-      $logo_type: "image",
-      logo: "",
-      url: ""
-    }
-  ],
-  tags: [
-    {
-      nombre_del_tag: "Dirección",
-      $nombre_del_tag_en: "Direction"
-    },
-    {
-      nombre_del_tag: "Desarrollo",
-      $nombre_del_tag_en: "Develop"
-    },
-    {
-      nombre_del_tag: "Guión",
-      $nombre_del_tag_en: "Script"
-    },
-    {
-      nombre_del_tag: "Cinematografía",
-      $nombre_del_tag_en: "Cinematography"
-    },
-    {
-      nombre_del_tag: "Producción",
-      $nombre_del_tag_en: "Production"
-    },
-    {
-      nombre_del_tag: "Música",
-      $nombre_del_tag_en: "Music"
-    },
-    {
-      nombre_del_tag: "Casting",
-      $nombre_del_tag_en: "Casting"
-    },
-    {
-      nombre_del_tag: "Edición",
-      $nombre_del_tag_en: "Editing"
-    },
-    {
-      nombre_del_tag: "Diseño de vestuario",
-      $nombre_del_tag_en: "Costume Design"
-    },
-    {
-      nombre_del_tag: "Arte",
-      $nombre_del_tag_en: "Art"
-    },
-    {
-      nombre_del_tag: "Maquillaje",
-      $nombre_del_tag_en: "Makeup"
-    },
-    {
-      nombre_del_tag: "Fotografía",
-      $nombre_del_tag_en: "Photography"
-    },
-    {
-      nombre_del_tag: "Animación",
-      $nombre_del_tag_en: "Animation"
-    },
-    {
-      nombre_del_tag: "Identificación de la marca",
-      $nombre_del_tag_en: "Brand Id"
-    }
-  ],
-  $titulo_proyecto_en: "UNTIL I KNEW YOU",
-  $tipo_de_proyecto_en: "tv",
-  $imdb_en: "https://www.imdb.com/title/tt5163162/",
-  $descripcion_corta_proyecto_en:
-    "THE HISTORY OF AN EXTRAORDINARY BEING THAT TRANSFORMED HIS DESTINY TO BECOME THE MOST LOVED CANTABTOR OF IBEROAMÉRICA.",
-  $texto_sinopsis_en:
-    '"Until I met you" a series that follows the footsteps of Alberto Aguilera Valadéz towards a dream of fame. A journey of 13 autobiographical episodes based on unpublished testimonies that unveil, as never before, the life of the Divo de Juárez: Juan Gabriel. A story that tells how his extraordinary talent led this singer to challenge his destiny and overcome poverty, betrayals and prejudices until he became "Juanga", the most beloved musical icon of Latin America.',
-  $texto_directores_en: "directors",
-  $ver_proyecto_texto_en: "see proyect",
-  $trailer_url_en: "https://www.youtube.com/watch?v=y1ITX16k-qs&amp;index=1"
-});
+	id: '4',
+	titulo_proyecto: 'HASTA QUE TE CONOCÍ',
+	tagline_proyecto: '',
+	tipo_de_proyecto: 'tv',
+	$logo_proyecto_type: 'image',
+	logo_proyecto: '/remote/direct_uploads/1545279268_1537216060_hqtc_logo.png',
+	imdb: 'https://www.imdb.com/title/tt5163162/',
+	$imagen_homepage_type: 'image',
+	imagen_homepage: '/remote/direct_uploads/1545279291_Home_HQTC.jpg',
+	$descripcion_corta_proyecto_type: 'textarea',
+	descripcion_corta_proyecto: 'LA HISTORIA DE UN SER EXTRAORDINARIO QUE TRANSFORMÓ SU DESTINO PARA CONVERTIRSE EN EL CANTAUTOR MÁS QUERIDO DE IBEROAMÉRICA.',
+	$video_fullscreen_type: 'file',
+	video_fullscreen: '',
+	genero: {
+		nombre: 'género',
+		texto: 'biodrama',
+		$nombre_en: 'genre',
+		$texto_en: 'biopic'
+	},
+	formato: {
+		nombre: 'formato',
+		texto: '13 x 45\'',
+		$nombre_en: 'format',
+		$texto_en: '13x45’'
+	},
+	idioma: {
+		nombre: 'idioma',
+		texto: 'español',
+		$nombre_en: 'lenguage',
+		$texto_en: 'spanish'
+	},
+	episodios: {
+		nombre: 'episodios',
+		texto: '13',
+		$nombre_en: 'episodes',
+		$texto_en: '13'
+	},
+	cliente: {
+		nombre: 'cliente',
+		texto: 'disney',
+		$logotipo_del_cliente_type: 'image',
+		logotipo_del_cliente: '',
+		$nombre_en: 'client',
+		$texto_en: 'DISNEY (IMG)'
+	},
+	galeria_sinopsis: [
+		{
+			$imagen_sinopsis_type: 'image',
+			imagen_sinopsis: '/remote/direct_uploads/1545279357_HQTC_1.jpg',
+			texto_imagen: ''
+		}
+	],
+	$texto_sinopsis_type: 'textarea',
+	texto_sinopsis: '“Hasta que te conocí” una serie que sigue los pasos de Alberto Aguilera Valadéz hacia un sueño de fama. Un recorrido de 13 episodios autobiográficos basados en testimonios inéditos que develan como nunca antes, la vida del Divo de Juárez: Juan Gabriel. Una historia que narra cómo su extraordinario talento llevó a este cantautor a desafiar su destino y superar la pobreza, traiciones y prejuicios hasta convertirse en “Juanga”, el ícono musical más querido de Iberoamérica.',
+	texto_directores: 'directores',
+	directores: [
+		{
+			nombre: 'Álvaro Curiel',
+			$nombre_en: 'Álvaro Curiel'
+		},
+		{
+			nombre: 'Rigoberto Castañeda ',
+			$nombre_en: 'Rigoberto Castañeda'
+		},
+		{
+			nombre: 'Alfonso Pineda Ulloa',
+			$nombre_en: 'Alfonso Pineda Ulloa'
+		},
+		{
+			nombre: 'Alejandro Aimetta',
+			$nombre_en: 'Alejandro Aimetta'
+		}
+	],
+	texto_actores: 'cast principal',
+	actores: [
+		{
+			nombre: 'Julián Román',
+			$nombre_en: 'Julián Román'
+		},
+		{
+			nombre: 'Dolores Heredia ',
+			$nombre_en: 'Dolores Heredia '
+		},
+		{
+			nombre: 'Marco Treviño',
+			$nombre_en: 'Marco Treviño'
+		},
+		{
+			nombre: 'Irán Castillo',
+			$nombre_en: 'Irán Castillo'
+		},
+		{
+			nombre: 'Verónica Merchant ',
+			$nombre_en: 'Verónica Merchant '
+		},
+		{
+			nombre: 'Ernesto Gómez Cruz ',
+			$nombre_en: 'Ernesto Gómez Cruz '
+		},
+		{
+			nombre: 'Isabel Burr',
+			$nombre_en: 'Isabel Burr'
+		},
+		{
+			nombre: 'Carlos Elias Yorvick',
+			$nombre_en: 'Carlos Elias Yorvick'
+		},
+		{
+			nombre: 'Damayanti Quintanar',
+			$nombre_en: 'Damayanti Quintanar'
+		},
+		{
+			nombre: 'Verónica Langer',
+			$nombre_en: 'Verónica Langer'
+		},
+		{
+			nombre: 'Luz Treviño',
+			$nombre_en: 'Luz Treviño'
+		},
+		{
+			nombre: 'Luz Ramos ',
+			$nombre_en: 'Luz Ramos '
+		},
+		{
+			nombre: 'Geraldine Galván ',
+			$nombre_en: 'Geraldine Galván '
+		},
+		{
+			nombre: 'Alejandro Calva ',
+			$nombre_en: 'Alejandro Calva '
+		},
+		{
+			nombre: 'Tenoch Huerta ',
+			$nombre_en: 'Tenoch Huerta '
+		},
+		{
+			nombre: 'Andrés Palacios',
+			$nombre_en: 'Andrés Palacios'
+		}
+	],
+	trailer_url: 'https://www.youtube.com/embed/y1ITX16k-qs',
+	ver_proyecto_texto: 'ver proyecto',
+	imagenes_backstage: [
+		{
+			$imagen_type: 'image',
+			imagen: ''
+		}
+	],
+	$premios_min: 1,
+	premios: [
+		{
+			titulo: '',
+			$logo_type: 'image',
+			logo: '/remote/direct_uploads/1545522116_emmy.svg',
+			url: ''
+		}
+	],
+	tags: [
+		{
+			nombre_del_tag: 'Dirección',
+			$nombre_del_tag_en: 'Direction'
+		},
+		{
+			nombre_del_tag: 'Desarrollo',
+			$nombre_del_tag_en: 'Develop'
+		},
+		{
+			nombre_del_tag: 'Guión',
+			$nombre_del_tag_en: 'Script'
+		},
+		{
+			nombre_del_tag: 'Cinematografía',
+			$nombre_del_tag_en: 'Cinematography'
+		},
+		{
+			nombre_del_tag: 'Producción',
+			$nombre_del_tag_en: 'Production'
+		},
+		{
+			nombre_del_tag: 'Música',
+			$nombre_del_tag_en: 'Music'
+		},
+		{
+			nombre_del_tag: 'Casting',
+			$nombre_del_tag_en: 'Casting'
+		},
+		{
+			nombre_del_tag: 'Edición',
+			$nombre_del_tag_en: 'Editing'
+		},
+		{
+			nombre_del_tag: 'Diseño de vestuario',
+			$nombre_del_tag_en: 'Costume Design'
+		},
+		{
+			nombre_del_tag: 'Arte',
+			$nombre_del_tag_en: 'Art'
+		},
+		{
+			nombre_del_tag: 'Maquillaje',
+			$nombre_del_tag_en: 'Makeup'
+		},
+		{
+			nombre_del_tag: 'Fotografía',
+			$nombre_del_tag_en: 'Photography'
+		},
+		{
+			nombre_del_tag: 'Animación',
+			$nombre_del_tag_en: 'Animation'
+		},
+		{
+			nombre_del_tag: 'Identificación de la marca',
+			$nombre_del_tag_en: 'Brand Id'
+		}
+	],
+	$titulo_proyecto_en: 'UNTIL I KNEW YOU',
+	$tipo_de_proyecto_en: 'tv',
+	$imdb_en: 'https://www.imdb.com/title/tt5163162/',
+	$descripcion_corta_proyecto_en: 'THE HISTORY OF AN EXTRAORDINARY BEING THAT TRANSFORMED HIS DESTINY TO BECOME THE MOST LOVED CANTABTOR OF IBEROAMÉRICA.',
+	$texto_sinopsis_en: '"Until I met you" a series that follows the footsteps of Alberto Aguilera Valadéz towards a dream of fame. A journey of 13 autobiographical episodes based on unpublished testimonies that unveil, as never before, the life of the Divo de Juárez: Juan Gabriel. A story that tells how his extraordinary talent led this singer to challenge his destiny and overcome poverty, betrayals and prejudices until he became "Juanga", the most beloved musical icon of Latin America.',
+	$texto_directores_en: 'directors',
+	$ver_proyecto_texto_en: 'see proyect',
+	$trailer_url_en: 'https://www.youtube.com/watch?v=y1ITX16k-qs&amp;index=1'
+})
