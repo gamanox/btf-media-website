bf81d45492d6044371704e034bc7e454e5918204
willalvarez <will.alvarez@gmail.com>
2019-01-02 10:42:17 -0600

    menu: esconder logo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
     $(this).toggleClass("active");
     $("#menu").toggleClass("active");
     $(".btn-politicas").toggleClass("active");
-    $(".logo_small_white").toggleClass("active");
+    // $(".logo_small_white").toggleClass("active");
   });
   //boton politicas de privacidad
   $(".btn-politicas").on("click", function(event) {
